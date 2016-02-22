4ad7cbd9c3eb229f3cf04cd324814e321e792825
Taylor Wicksell <twicksell@netflix.com>
2016-02-22 10:31:21 -0800

    added HealthStatusServlet as a temporary implementation for exposing health via RESt

---
+++ health-integrations/src/main/java/com/netflix/runtime/health/servlet/HealthStatusServlet.java
@@ -0,0 +1,47 @@
+package com.netflix.runtime.health.servlet;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.netflix.runtime.health.api.HealthCheckAggregator;
+import com.netflix.runtime.health.api.HealthCheckStatus;
+
+@Singleton
+public class HealthStatusServlet extends HttpServlet {
+    
+    private static final long serialVersionUID = -6518168654611266480L;
+    private final HealthCheckAggregator healthCheckAggregator;
+
+    @Inject
+    public HealthStatusServlet(HealthCheckAggregator healthCheckAggregator) {
+        this.healthCheckAggregator = healthCheckAggregator;
+    }
+    
+    @Override
+    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws IOException, ServletException {
+        HealthCheckStatus health;
+        try {
+            health = this.healthCheckAggregator.check().get();
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+        
+        if(health.isHealthy()) {
+            resp.setStatus(200);
+        }
+        else {
+            resp.setStatus(500);
+        }
+        String content = health.toString();
+        resp.setContentLength(content.length());
+        resp.setContentType("text/plain");
+        resp.getWriter().print(content);
+    }
+
+}
