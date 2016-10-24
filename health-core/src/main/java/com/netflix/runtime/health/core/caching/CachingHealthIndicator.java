8013a003c761e7c8f9bd4640a813b7aebd3bbb41
Thomas A Cellucci <tcellucci@netflix.com>
2016-10-24 11:10:34 -0700

    fix formatting

--- health-core/src/main/java/com/netflix/runtime/health/core/caching/CachingHealthIndicator.java
+++ health-core/src/main/java/com/netflix/runtime/health/core/caching/CachingHealthIndicator.java
@@ -1,23 +1,18 @@
 /**
  * Copyright 2016 Netflix, Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package com.netflix.runtime.health.core.caching;
 
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.StampedLock;
 
 import com.netflix.runtime.health.api.Health;
@@ -31,70 +26,69 @@ import com.netflix.runtime.health.api.HealthIndicatorCallback;
  *
  */
 public class CachingHealthIndicator implements HealthIndicator {
-	private static class CacheEntry {
-		private final long expirationTime;
-		private final Health health;
+    private static class CacheEntry {
+        private final long expirationTime;
+        private final Health health;
 
-		CacheEntry(long expirationTime, Health health) {
-			super();
-			this.expirationTime = expirationTime;
-			this.health = health;
-		}
+        CacheEntry(long expirationTime, Health health) {
+            super();
+            this.expirationTime = expirationTime;
+            this.health = health;
+        }
 
-		long getExpirationTime() {
-			return expirationTime;
-		}
+        long getExpirationTime() {
+            return expirationTime;
+        }
 
-		Health getHealth() {
-			return health;
-		}
-	}
+        Health getHealth() {
+            return health;
+        }
+    }
 
-	private final StampedLock lock;
-	private final long interval;
-	private final HealthIndicator delegate;
-	private volatile CacheEntry cachedHealth;
+    private final StampedLock lock;
+    private final long interval;
+    private final HealthIndicator delegate;
+    private volatile CacheEntry cachedHealth;
 
-	private CachingHealthIndicator(HealthIndicator delegate, long interval, TimeUnit units) {
-		this.delegate = delegate;
-		this.interval = TimeUnit.NANOSECONDS.convert(interval, units);
-		this.lock = new StampedLock();
-		this.cachedHealth = new CacheEntry(0L, null);
-	}
+    private CachingHealthIndicator(HealthIndicator delegate, long interval, TimeUnit units) {
+        this.delegate = delegate;
+        this.interval = TimeUnit.NANOSECONDS.convert(interval, units);
+        this.lock = new StampedLock();
+        this.cachedHealth = new CacheEntry(0L, null);
+    }
 
-	@Override
-	public void check(HealthIndicatorCallback callback) {
-		CacheEntry cacheEntry = this.cachedHealth;		
-		long stamp = lock.tryReadLock();		
-		if (stamp != 0L) {
-			long currentTime = System.nanoTime();
-			if (currentTime > cacheEntry.getExpirationTime()) {
-				stamp = lock.tryConvertToWriteLock(stamp);
-				if (stamp != 0L) {
-					try {
-						delegate.check(h -> {
-							this.cachedHealth = new CacheEntry(
-									currentTime + interval,
-									Health.from(h).withDetail(Health.CACHE_KEY, true).build());
-							callback.inform(h);
-						});
-						return;
-					} finally {
-						lock.unlockWrite(stamp);
-					}
-				}
-			} else {
-				lock.unlockRead(stamp);
-			}
-		}
-		callback.inform(cacheEntry.getHealth());
-	}
+    @Override
+    public void check(HealthIndicatorCallback callback) {
+        CacheEntry cacheEntry = this.cachedHealth;
+        long stamp = lock.tryReadLock();
+        if (stamp != 0L) {
+            long currentTime = System.nanoTime();
+            if (currentTime > cacheEntry.getExpirationTime()) {
+                stamp = lock.tryConvertToWriteLock(stamp);
+                if (stamp != 0L) {
+                    try {
+                        delegate.check(h -> {
+                            this.cachedHealth = new CacheEntry(currentTime + interval,
+                                    Health.from(h).withDetail(Health.CACHE_KEY, true).build());
+                            callback.inform(h);
+                        });
+                        return;
+                    } finally {
+                        lock.unlockWrite(stamp);
+                    }
+                }
+            } else {
+                lock.unlockRead(stamp);
+            }
+        }
+        callback.inform(cacheEntry.getHealth());
+    }
 
-	public static CachingHealthIndicator wrap(HealthIndicator delegate, long interval, TimeUnit units) {
-		return new CachingHealthIndicator(delegate, interval, units);
-	}
+    public static CachingHealthIndicator wrap(HealthIndicator delegate, long interval, TimeUnit units) {
+        return new CachingHealthIndicator(delegate, interval, units);
+    }
 
-	public String getName() {
-		return delegate.getName();
-	}
+    public String getName() {
+        return delegate.getName();
+    }
 }
